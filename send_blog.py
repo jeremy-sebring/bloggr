4540dea7efc75bb657b977d2c35bf69d3b77bccd
jeremy-sebring <10858588+jeremy-sebring@users.noreply.github.com>
2024-02-23 10:10:48 -0500

    final poc

--- send_blog.py
+++ send_blog.py
@@ -2,27 +2,29 @@ import argparse
 import requests
 from dotenv import load_dotenv
 import os
+import frontmatter
 
 from pathlib import Path as p
 
-# Load environment variables from a .env file
+
 load_dotenv()
 
-# Parse command-line arguments
 parser = argparse.ArgumentParser(description="Send a Markdown file as a string in a POST request to an API.")
 parser.add_argument("file", help="Path to the Markdown file to be sent")
 parser.add_argument("-d", action="store_true", help="Send to Dev")
 parser.add_argument("-p", action="store_true", help="Send to Prod")
 args = parser.parse_args()
 
-# Read the content of the Markdown file
-try:
-    file = p(args.file)
-    with open(p, 'r') as file:
-        markdown_content = file.read()
 
-    # Replace new line characters with \n
-    markdown_content = markdown_content.replace("\n", "\\n")
+try:
+    newFile = p(args.file)
+    with open(newFile, 'r') as file:
+        post = frontmatter.load(file)
+
+    markdown_content = post.content.replace("\n", "\\n")
+   
+    post = dict(post)
+
 
     # Retrieve the API-Key from environment variables
     api_key = os.getenv("API_Key")
@@ -45,29 +47,38 @@ try:
     else:
         print("Please specify -d or -p")
         exit(1)
-    # Define the API URL
-    # Change this to your actual API URL
 
-    # Set up the headers including the API-Key
+
     headers = {
         "Content-Type": "application/json",
         "API-Key": f"{api_key}"
     }
 
-    # Prepare the data payload
-    data = {"markdown": markdown_content}
-    print(data)
+    response = requests.get(api_url, headers=headers)
 
-    '''
-    # Send the POST request
+    json_response = dict(response.json())
+
+    for item in json_response['data']:
+        if item['slug'] == newFile.name.replace(".md", ""):
+            api_url = api_url + "/" + item['slug']
+
+
+
+ 
+    data = {
+        "title":  post["title"],
+        "markdown": markdown_content,
+        "slug": newFile.name.replace(".md", ""),
+        "description": post["description"]
+        }
+    
     response = requests.post(api_url, json=data, headers=headers)
 
-    # Check the response
+
     if response.status_code == 200:
         print("Markdown content successfully sent to the API.")
     else:
         print(f"Failed to send Markdown content. Status code: {response.status_code}, Message: {response.text}")
-    '''
 
 except FileNotFoundError:
     print("The specified file does not exist.")
